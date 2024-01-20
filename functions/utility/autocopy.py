7645b7b5541f6571ed35dcb45e1a53939779149f
Mana64 <madsbrriinckbastholm@gmail.com>
2024-01-20 17:30:22 +0100

    auto copy added

---
+++ functions/utility/autocopy.py
@@ -0,0 +1,45 @@
+from .filesys import *
+import threading
+import keyboard
+import pyperclip
+
+data = readfile(settings_file)
+
+COPY_CODES_KEY = data["COPY_CODES_KEY"]
+
+word_list = [
+"UPD4",
+"SORRYDELAYS3",
+"100MVISITSTHANKS",
+"SEASONONE",
+"SORRYDELAYS2",
+"SORRYDELAYS:(",
+"BETA",
+"XMAS",
+"FOLLOWER",
+]
+
+index = 0
+
+def copy_next_word(event):
+    global index
+    if event.name == COPY_CODES_KEY:  # you can change 'f' to any key
+        if index < len(word_list):
+            word_to_copy = word_list[index]
+            index += 1
+            pyperclip.copy(word_to_copy)
+            # print(f"Copied: {word_to_copy}")
+        else:
+            index = 0
+            # print("End of the list. Restarting...")
+
+def main_copy():
+    keyboard.on_press(copy_next_word)
+
+    keyboard.wait('f5')
+
+auto_copy = data["AUTO_COPY_CODES"]
+
+if auto_copy:
+    thread_one = threading.Thread(target=main_copy)
+    thread_one.start()
